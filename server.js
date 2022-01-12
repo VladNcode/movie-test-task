dc3c1fdc09f00cb54ab62e3538b426825ca6dd76
VladNCode <vldsmrfow@gmail.com>
2022-01-12 06:43:42 +0200

    built skeleton

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config({ path: './config.env' });
+
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION 💥 REJECTED REJECTED REJECTED');
+  process.exit(1);
+});
+
+const app = require('./app');
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`Listening at port ${port}`);
+  console.log(`Currently in: ${process.env.NODE_ENV}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION 💥 REJECTED REJECTED REJECTED');
+  server.close(() => {
+    process.exit(1);
+  });
+});
